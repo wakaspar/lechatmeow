de7faf938b0952cf7093b394728e9bb75c2d3d31
Will Kaspar <Will@Aang.local>
2017-06-09 17:37:51 -0700

    more of the same - prod env ts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,15 +23,15 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
-  config.action_cable.url = "wss://#{hostname}/cable"
-  config.action_cable.allowed_request_origins = ['https://lechatmeow.herokuapp.com']
-
-  ### GHOST CODE ###
+  config.web_socket_server_url = "wss://lechatmeow.herokuapp.com/cable"
   # config.action_cable.url = 'wss://lechatmeow.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = ['https://lechatmeow.herokuapp.com', 'http://lechatmeow.herokuapp.com']
+
+
 
 
 
